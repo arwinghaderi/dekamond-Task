71c50d34f2fea72a419233ac45379a6360282b6e
arwinGhaderi <arwinghaderi81@gmail.com>
2025-08-05 01:02:33 +0330

    feat: add UserContext with persistent localStorage integration

---
+++ src/context/UserContext.tsx.tsx
@@ -0,0 +1,37 @@
+'use client'
+import {
+  createContext,
+  useContext,
+  useState,
+  ReactNode,
+  useEffect,
+} from 'react'
+
+import { User } from '@/types/user'
+
+type UserContextType = {
+  user: User | null
+  setUser: (user: User) => void
+}
+
+const UserContext = createContext<UserContextType>({
+  user: null,
+  setUser: () => {},
+})
+
+export const useUser = () => useContext(UserContext)
+
+export const UserProvider = ({ children }: { children: ReactNode }) => {
+  const [user, setUser] = useState<User | null>(null)
+
+  useEffect(() => {
+    const stored = localStorage.getItem('user')
+    if (stored) setUser(JSON.parse(stored))
+  }, [])
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  )
+}
